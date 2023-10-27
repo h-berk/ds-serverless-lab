506d9d1f68010e8c29e991080c15db53d5b40695
Hasan Berk <hasanberk50@gmail.com>
2023-10-27 09:35:20 +0100

    Resource to query cast members.

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -1,4 +1,3 @@
-
 export type Movie = {
     movieId: number;
     genre_ids: number[];
@@ -10,4 +9,17 @@ export type Movie = {
     video: boolean;
     vote_average: number;
     vote_count: number
+  }
+
+  export type MovieCast = {
+    movieId: number;
+    actorName: string;
+    roleName: string;
+    roleDescription: string;
+  };
+  // Used to validate the query string og HTTP Get requests
+  export type MovieCastMemberQueryParams = {
+    movieId: string;
+    actorName?: string;
+    roleName?: string
   }
